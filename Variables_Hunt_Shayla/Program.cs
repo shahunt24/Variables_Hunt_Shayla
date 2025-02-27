39ba2f1441442ec4eac27d5aab4c63f6445c9d05
Shayla <Shaylahunt834@gmail.com>
2025-02-27 12:50:07 -0600

    Add project files.

---
+++ Variables_Hunt_Shayla/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Variables_Hunt_Shayla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
