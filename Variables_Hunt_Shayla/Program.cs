4429e9c05f828e4eb4fb1689137280e2386720eb
Shayla <Shaylahunt834@gmail.com>
2025-03-07 13:53:02 -0600

    Update Program.cs

--- Variables_Hunt_Shayla/Program.cs
+++ Variables_Hunt_Shayla/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favoriteNumber; // Declares an interer to store a favorite number.
+            bool isJumping = false, isRunning = false; // Booleans intialized to false for jumoung and running.
+            float floatyFloat; // Declared random float variable.
+            favoriteNumber = 8; // Assigning floatyFloat to 8
+            floatyFloat = 31.14f; // Assinging floatyFloat to 31.14
+            const double finalGrade = 75.0; // Initializing constant finalGrade to 90
+
+            // Prints all variables to console.
+            Console.WriteLine(favoriteNumber);
+            Console.WriteLine(isJumping);
+            Console.WriteLine(isRunning);
+            Console.WriteLine(floatyFloat);
+            Console.WriteLine(finalGrade);
         }
     }
 }
